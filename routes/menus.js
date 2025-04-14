ffc20e0562addfb2331ce64de55e5e9cb920ee66
taisenish <131194085+taisenish@users.noreply.github.com>
2025-04-13 23:40:56 -0700

    Update menus.js

--- routes/menus.js
+++ routes/menus.js
@@ -2,7 +2,7 @@ import menusDessertsRouter from './menus_desserts.js'
 import express from 'express'
 const router = express.Router()
 
-router.get('/', (req, res) +> {
+router.get('/', (req, res) => {
     res.send("this is a menu")
 })
 
