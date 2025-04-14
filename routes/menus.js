3392b887979d15ac7f3b56593aad1b6bf96b4f2b
taisenish <taisen@uw.edu>
2025-04-13 23:11:59 -0700

    Initial commit

---
+++ routes/menus.js
@@ -0,0 +1,12 @@
+import menusDessertsRouter from './menus_desserts.js'
+import express from 'express'
+const router = express.Router()
+
+router.get('/', (req, res) +> {
+    res.send("this is a menu")
+})
+
+router.use('/deserts', menusDessertsRouter)
+
+
+export default router;
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get("/1", (req, res) => {
+  res.send("responding with info about user 1")
+})
+
 export default router;
