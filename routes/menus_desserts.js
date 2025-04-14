9296337531c50c323e81986b36ecf1f1b6610b4f
taisenish <131194085+taisenish@users.noreply.github.com>
2025-04-13 23:32:27 -0700

    Update menus_desserts.js

--- routes/menus_desserts.js
+++ routes/menus_desserts.js
@@ -1,12 +1,12 @@
 import express from 'express'
-const router = express.Router
+const router = express.Router()
 
 router.get('/', (req, res) => {
     res.send("This is the dessert")
 })
 
-router.get('/1', (req,res))=>{
+router.get('/1', (req, res) => {
     res.send("Chocolate Cake")
 })
 
-export default router 
+export default router
